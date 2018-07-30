7b7d9786135f1abd8916dcecf744db964d1b8b24
Kpexa <33284586+Kpexa@users.noreply.github.com>
2018-07-30 13:33:42 +0300

    can use decimals

--- scripts.js
+++ scripts.js
@@ -76,11 +76,10 @@ window.addEventListener('load', function(){
 
   function validation() {
     // функція валідації поля вводу довжини (не допускає ввід дробових чисел, символів та букв)
-    var pattern = /^(-)?[0-9]+$/;
-
+    var pattern = /^(-)?[0-9]+$/; 
     if(!pattern.test(this.value)) {      
-      var new_value = this.value.replace(/[^0-9]/g, '');
-      this.value = new_value;      
+      var new_value = this.value.replace(/[^0-9.]/g, '');      
+      this.value = new_value;
     }
 
   };
